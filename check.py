8a33a770d3d7fc91f64f7f01364fa027ba336f13
Wal1et <whqxbs@163.com>
2024-08-30 16:37:00 +0800

    Add files via upload

---
+++ check.py
@@ -0,0 +1,11 @@
+import sys
+
+# 获取输入参数
+number1 = float(sys.argv[1])
+number2 = float(sys.argv[2])
+
+# 计算和
+result = number1 + number2
+
+# 输出结果
+print(f"The sum of {number1} and {number2} is {result}")
